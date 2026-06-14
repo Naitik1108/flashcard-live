200f5c23f634bf54b6771a24f37f49f1c2ea1aa8
Naitik1108 <naitikshah.coder@gmail.com>
2026-06-14 17:19:43 +0530

    Create src/lib/constants/routes.ts

---
+++ src/lib/constants/routes.ts
@@ -0,0 +1,10 @@
+export const ROUTES = {
+  HOME: "/",
+  LOGIN: "/login",
+  SIGNUP: "/signup",
+  DASHBOARD: "/dashboard",
+  DECKS: "/decks",
+  PUBLIC_DECKS: "/public-decks",
+  HISTORY: "/history",
+  PROFILE: "/profile",
+};
