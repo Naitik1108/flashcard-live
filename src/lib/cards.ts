291adf9b890eefabd22cf0aab56dc0936c02adba
Naitik1108 <naitikshah.coder@gmail.com>
2026-06-16 14:43:15 +0530

    Update cards.ts

--- src/lib/cards.ts
+++ src/lib/cards.ts
@@ -16,11 +16,49 @@ export async function createCard(
     .single();
 }
 
+export async function createManyCards(
+  cards: {
+    deck_id: string;
+    question: string;
+    answer: string;
+  }[]
+) {
+  return supabase
+    .from("cards")
+    .insert(cards);
+}
+
 export async function getDeckCards(
   deckId: string
 ) {
   return supabase
     .from("cards")
     .select("*")
-    .eq("deck_id", deckId);
+    .eq("deck_id", deckId)
+    .order("created_at", {
+      ascending: true,
+    });
+}
+
+export async function updateCard(
+  cardId: string,
+  question: string,
+  answer: string
+) {
+  return supabase
+    .from("cards")
+    .update({
+      question,
+      answer,
+    })
+    .eq("id", cardId);
+}
+
+export async function deleteCard(
+  cardId: string
+) {
+  return supabase
+    .from("cards")
+    .delete()
+    .eq("id", cardId);
 }
