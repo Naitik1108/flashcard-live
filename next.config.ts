c0ed3642c3be1be610b1ae31af27d98b6433f741
Naitik1108 <naitikshah.coder@gmail.com>
2026-06-16 14:05:13 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
+import withPWA from "next-pwa";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+
+  images: {
+    remotePatterns: [],
+  },
+
+  experimental: {
+    optimizePackageImports: [
+      "lucide-react"
+    ]
+  }
 };
 
-export default nextConfig;
+export default withPWA({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true,
+})(nextConfig);
